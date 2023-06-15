03516b7222e7477039631ff9d275faf2bdbbc1dc
zz <35785077@qq.com>
2023-06-15 20:36:47 +0800

    qmake index

--- qt_cmake/mainwindow.cpp
+++ qt_cmake/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "IndexWidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent)
     QString title = QString("qt_cmake V%1").arg("0.0.1");
     setWindowTitle(title);
     setStyleSheet(QString(".MainWindow{background-color:%1;}").arg("rgb(31,33,42)"));
+
+    IndexWidget *index = new IndexWidget(this);
+    setCentralWidget(index);
 }
 
 MainWindow::~MainWindow()
