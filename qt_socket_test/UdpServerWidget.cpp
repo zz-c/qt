933b23a055d7147c99ff4ac35c7864ed482fa783
zz <35785077@qq.com>
2023-06-24 21:57:50 +0800

    udpserver

--- qt_socket_test/UdpServerWidget.cpp
+++ qt_socket_test/UdpServerWidget.cpp
@@ -2,6 +2,7 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QHostInfo>
 
 UdpServerWidget::UdpServerWidget(QWidget *parent) : QWidget(parent)
 {
@@ -9,10 +10,82 @@ UdpServerWidget::UdpServerWidget(QWidget *parent) : QWidget(parent)
     mainVLayout->setContentsMargins(0,0,0,0);
     mainVLayout->setSpacing(0);
 
-    QLabel *label = new QLabel(this);
-    label->setStyleSheet(".QLabel{color:rgb(255,255,255);}");
-    label->setText("UdpServerWidget-开发中");
+    infolabel = new QLabel(this);
+    infolabel->setStyleSheet(".QLabel{color:rgb(255,255,255);font-size:34px;}");
+    infolabel->setText("点击开始");
+
+    countLabel = new QLabel(this);
+    countLabel->setStyleSheet(".QLabel{color:rgb(255,255,255);font-family:Microsoft YaHei;font-size:34px;}");
+    countLabel->setText(QString("统计数据 "));
+
+    QWidget * recordWidget = new QWidget(this);
+    recordWidget->setFixedHeight(80);
+    QHBoxLayout *recordHLayout = new QHBoxLayout(recordWidget);
+    recordHLayout->setContentsMargins(0,0,0,0);
+    recordHLayout->setSpacing(0);
+
+    QLabel *fileNameLabel = new QLabel(this);
+    fileNameLabel->setFixedWidth(60);
+    fileNameLabel->setStyleSheet(".QLabel{color:rgb(255,255,255);font-size:15px;}");
+    fileNameLabel->setText("ip");
+
+    ipLineEdit = new QLineEdit(this);
+    ipLineEdit->setFixedHeight(32);
+    ipLineEdit->setPlaceholderText("服务端ip");
+    ipLineEdit->setStyleSheet(".QLineEdit{color:rgb(0,0,0);font-size:15px;border:1px solid rgb(217,217,217);border-radius: 3px; padding: 1px;}\
+                               .QLineEdit:hover {border:1px solid rgb(64,65,66);}");
+    ipLineEdit->setClearButtonEnabled(true);
+    ipLineEdit->setFocusPolicy(Qt::FocusPolicy::ClickFocus);
+    ipLineEdit->setCursorPosition(0);
+    ipLineEdit->setText(getLocalIP());
+
+    startBtn = new QPushButton(recordWidget);
+    startBtn->setFixedSize(120,40);
+    startBtn->setCursor(Qt::PointingHandCursor);
+    startBtn->setText("开始");
+
+    udpSocket=new QUdpSocket(this);//用于与连接的客户端通讯的QTcpSocket
+    connect(startBtn,&QPushButton::clicked,this,[this](){
+        QString targetIP=ipLineEdit->text();
+        QHostAddress targetAddr(targetIP);  //目标IP
+        quint16 targetPort = 9998;  //目标端口号
+        QString msg = "发送内容"; //发送内容
+        QByteArray byteArray = msg.toUtf8();
+
+        udpSocket->writeDatagram(byteArray, targetAddr, targetPort);  //发送函数
+
+
+    });
+
+
+    recordHLayout->addWidget(fileNameLabel);
+    recordHLayout->addWidget(ipLineEdit);
+    recordHLayout->addWidget(startBtn);
+
     mainVLayout->addSpacing(50);
-    mainVLayout->addWidget(label,0,Qt::AlignHCenter);
+    mainVLayout->addWidget(infolabel,0,Qt::AlignHCenter);
     mainVLayout->addStretch(50);
+    mainVLayout->addWidget(countLabel);
+    mainVLayout->addStretch(50);
+    mainVLayout->addWidget(recordWidget);
+}
+
+QString UdpServerWidget::getLocalIP(){
+    QString hostName=QHostInfo::localHostName();//本地主机名
+    QHostInfo hostInfo=QHostInfo::fromName(hostName);
+    QString localIP="";
+
+    QList<QHostAddress> addList=hostInfo.addresses();//
+
+    if (!addList.isEmpty())
+        for (int i=0;i<addList.count();i++)
+        {
+            QHostAddress aHost=addList.at(i);
+            if (QAbstractSocket::IPv4Protocol==aHost.protocol())
+            {
+                localIP=aHost.toString();
+                break;
+            }
+        }
+    return localIP;
 }
--- qt_socket_test/UdpServerWidget.h
+++ qt_socket_test/UdpServerWidget.h
@@ -2,14 +2,26 @@
 #define UdpServerWidget_H
 
 #include <QWidget>
-
+#include <QPushButton>
+#include <QLabel>
+#include <QString>
+#include <QLineEdit>
+#include <QUdpSocket>
 
 class UdpServerWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit UdpServerWidget(QWidget *parent);
+private:
+    QLabel *infolabel;
+    QLabel *countLabel;
+    QLineEdit *ipLineEdit;
+    QPushButton *startBtn;
 
+    QUdpSocket *udpSocket;//
+
+    QString getLocalIP();//获取本机IP地址
 signals:
 
 };
