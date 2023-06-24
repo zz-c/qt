7884b02c64ff556985e15474d5ab9467f39b0818
zz <35785077@qq.com>
2023-06-24 12:17:59 +0800

    thread

--- qt_socket_test/TcpServerWidget.cpp
+++ qt_socket_test/TcpServerWidget.cpp
@@ -6,17 +6,24 @@
 #include <QSettings>
 #include <QFileDialog>
 
+void TcpServerWidget::run(){
+    while (true) {
+        qDebug()<<"run";
+        //std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
 TcpServerWidget::TcpServerWidget(QWidget *parent) : QWidget(parent)
 {
     QVBoxLayout *mainVLayout = new QVBoxLayout(this);
     mainVLayout->setContentsMargins(0,0,0,0);
     mainVLayout->setSpacing(0);
 
-    QLabel *label = new QLabel(this);
-    label->setStyleSheet(".QLabel{color:rgb(255,255,255);}");
-    label->setText("TcpServer功能-开发中111111111111111/n2222222222222222222222");
+    infolabel = new QLabel(this);
+    infolabel->setStyleSheet(".QLabel{color:rgb(255,255,255);}");
+    infolabel->setText("TcpServer功能-开发中111111111111111/n2222222222222222222222");
 
-    QLabel *countLabel = new QLabel(this);
+    countLabel = new QLabel(this);
     countLabel->setStyleSheet(".QLabel{color:rgb(0,0,0);font-family:Microsoft YaHei;font-size:14px;}");
     countLabel->setText(QString("当前版本 "));
 
@@ -33,7 +40,7 @@ TcpServerWidget::TcpServerWidget(QWidget *parent) : QWidget(parent)
     fileNameLabel->setStyleSheet(".QLabel{color:rgb(255,255,255);font-size:15px;}");
     fileNameLabel->setText("文件路径");
 
-    QLineEdit *fileLineEdit = new QLineEdit(this);
+    fileLineEdit = new QLineEdit(this);
     fileLineEdit->setFixedHeight(32);
     fileLineEdit->setPlaceholderText("请输入文件路径");
     fileLineEdit->setStyleSheet(".QLineEdit{color:rgb(0,0,0);font-size:15px;border:1px solid rgb(217,217,217);border-radius: 3px; padding: 1px;}\
@@ -50,12 +57,12 @@ TcpServerWidget::TcpServerWidget(QWidget *parent) : QWidget(parent)
                            .QPushButton:focus{outline: none;}");
     fileBtn->setFixedSize(100,40);
     fileBtn->setText("选择");
-    connect(fileBtn,&QPushButton::clicked,this,[this,fileLineEdit](){
+    connect(fileBtn,&QPushButton::clicked,this,[this](){
         //QString dirVal = QFileDialog::getExistingDirectory(this,"浏览","").trimmed();
         QString dirVal = QFileDialog::getOpenFileName(this, "选择要播放的文件","/","视频文件 (*.h264 *.flv *.rmvb *.avi *.mp4);; 所有文件 (*.*);; ");
         if(dirVal.length()>0){
-            fileLineEdit->setText(dirVal);
-            fileLineEdit->setCursorPosition(0);
+            this->fileLineEdit->setText(dirVal);
+            this->fileLineEdit->setCursorPosition(0);
             QSettings settings;
             settings.setValue("SETTINGS",dirVal);
         }
@@ -66,6 +73,13 @@ TcpServerWidget::TcpServerWidget(QWidget *parent) : QWidget(parent)
     startBtn->setFixedSize(120,40);
     startBtn->setCursor(Qt::PointingHandCursor);
     startBtn->setText("开始");
+    connect(startBtn,&QPushButton::clicked,this,[this](){
+        if(this->fileLineEdit->text().isEmpty()){
+            qDebug()<<"请选择文件";
+            infolabel->setText("请选择文件");
+        }
+        //std::thread thread(run,this);
+    });
 
     recordHLayout->addWidget(fileNameLabel);
     recordHLayout->addWidget(fileLineEdit);
@@ -74,7 +88,7 @@ TcpServerWidget::TcpServerWidget(QWidget *parent) : QWidget(parent)
 
 
     mainVLayout->addSpacing(50);
-    mainVLayout->addWidget(label,0,Qt::AlignHCenter);
+    mainVLayout->addWidget(infolabel,0,Qt::AlignHCenter);
     mainVLayout->addStretch(50);
     mainVLayout->addWidget(countLabel);
     mainVLayout->addStretch(50);
--- qt_socket_test/TcpServerWidget.h
+++ qt_socket_test/TcpServerWidget.h
@@ -3,14 +3,21 @@
 
 #include <QWidget>
 #include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
 
 class TcpServerWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit TcpServerWidget(QWidget *parent);
+
+    void run();
 private:
+    QLabel *infolabel;
+    QLabel *countLabel;
     QPushButton *startBtn;
+    QLineEdit *fileLineEdit;
 
 signals:
 
